e5664bcb68475afc1652718b283454b79d353fa5
karunya-krishna <kg8113@srmist.edu.in>
2026-02-24 14:23:28 +0530

    printing oops message with asteriks(*)

--- printclass.java
+++ printclass.java
@@ -1,6 +1,16 @@
 public class printclass {
 
 public static void main(String[] args) {
-System.out.println("oops");
+	System.out.println(" *****    *****    ******    ***** ");
+	System.out.println("*     *  *     *   *     *  *     *");
+    System.out.println("*     *  *     *   *     *  *      ");
+    System.out.println("*     *  *     *   ******    ***** ");
+    System.out.println("*     *  *     *   *              *");
+    System.out.println("*     *  *     *   *        *     *");
+    System.out.println(" *****    *****    *         ***** ");
+
+    }
 }
-}
+
+
+
