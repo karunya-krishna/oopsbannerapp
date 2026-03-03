1e665b161a73bb6cb550cbdbadf8c2adb8a4b0ed
karunya-krishna <kg8113@srmist.edu.in>
2026-03-03 14:31:57 +0530

    print oops using another method

--- printclass.java
+++ printclass.java
@@ -1,19 +1,54 @@
 public class printclass {
 
+    public static String[] getOPattern() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[] {
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] lines = {
-            " *****   *****   *****   ***** ",
-            "*     * *     * *     * *     *",
-            "*     * *     * *     * *      ",
-            "*     * *     * *****   ***** ",
-            "*     * *     * *            *",
-            "*     * *     * *     * *     *",
-            " *****   *****   *     * ***** "
-        };
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + "  " +
+                oPattern[i] + "  " +
+                pPattern[i] + "  " +
+                sPattern[i]
+            );
         }
     }
 }
